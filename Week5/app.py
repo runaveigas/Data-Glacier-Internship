b15cd97f8621a5e1e76155ad304411d1ca32c049
Veigas <runaveigas@gmail.com>
2021-07-09 14:47:23 +0530

    Heroku Deployment

--- Week5/app.py
+++ Week5/app.py
@@ -9,6 +9,7 @@ import numpy as np
 from flask import Flask, request, render_template
 import pickle
 
+
 app = Flask(__name__)
 model = pickle.load(open('model.pkl', 'rb'))
 
@@ -21,13 +22,13 @@ def predict():
     '''
     For rendering results on HTML GUI
     '''
-    int_features = [int(x) for x in request.form.values()]
+    int_features = [int(float(x)) for x in request.form.values()]
     final_features = [np.array(int_features)]
     prediction = model.predict(final_features)
 
-    output = round(prediction[0], 2)
-
-    return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format(output))
+    output = np.round(prediction[0], 2)
+ 
+    return render_template('index.html', prediction_text='The predicted Blood Pressure is {} mm Hg'.format(output))
 
 
 if __name__ == "__main__":
--- Week5/model.py
+++ Week5/model.py
@@ -11,17 +11,19 @@ import pickle
 from sklearn.linear_model import LinearRegression
 
 
-df = pd.read_csv('salary.csv')
-df['experience'].fillna(0,inplace = True)
+df = pd.read_excel('bloodpress.xlsx')
+#print(df.isnull().values.sum())
 
-X = df.iloc[:,:3]
-y = df.iloc[:,-1]
+X = df.iloc[:,1:]
+y = df.iloc[:,0:1]
 
 regressor = LinearRegression()
 regressor.fit(X,y)
 
+# Make predictions using the testing set
+bp_predict = regressor.predict(X)
+
+# Saving model to disk
 pickle.dump(regressor,open('model.pkl','wb'))
 
-model = pickle.load(open('model.pkl','rb'))
-print(model.predict([[2,5,6]]))
-
+    
